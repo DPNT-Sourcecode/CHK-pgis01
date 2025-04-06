e2f0024e6eefdc1518630f243e4185388c2e7088
sbx_user1051 <sbx_user1051@169.254.41.1>
2025-04-06 18:28:20 +0000

    Sun Apr 06 18:28:20 UTC 2025

--- test/solution_tests/CHK/test_chk1.py
+++ test/solution_tests/CHK/test_chk1.py
@@ -5,12 +5,24 @@ class TestCheckout():
     def test_checkout1_correct(self):
         assert CheckoutSolution().checkout("AAAB")==160
 
-    def test_checkout2_correct(self):
+    def test_checkout1_1_correct(self):
         assert CheckoutSolution().checkout("EEEBBE")==160
 
-    def test_checkout3_correct(self):
+    def test_checkout2E_correct(self):
         assert CheckoutSolution().checkout("ABCDECBAABCABBAAAEEAA")==665
         
+    def test_checkout3F_correct(self):
+        assert CheckoutSolution().checkout("F")==10
+        
+    def test_checkout3_1F_correct(self):
+        assert CheckoutSolution().checkout("FF")==20
 
+    def test_checkout3_2F_correct(self):
+        assert CheckoutSolution().checkout("FFF")==20
+
+    def test_checkout3_2F_correct(self):
+        assert CheckoutSolution().checkout("AFFF")==70
+        
     def test_checkout1_incorrect(self):
         assert CheckoutSolution().checkout("a")==-1
+
