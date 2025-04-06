8ab7e14c3c27752003930478f46bfe1c1b238696
sbx_user1051 <sbx_user1051@169.254.41.1>
2025-04-06 19:34:43 +0000

    Sun Apr 06 19:34:43 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -70,7 +70,7 @@ class CheckoutSolution:
                 if remainder >= 3:
                     total += (remainder // 3) * 130 
                     remainder = remainder % 3
-
+                print(items[item])
                 total += remainder * 50
 
             elif item == "B":
@@ -79,9 +79,20 @@ class CheckoutSolution:
             elif item == "F":
                 free_f = count // 3
                 total += (count-free_f) * items[item]
+            
+            if item == "H":
+                total += (count // 10) * 80
+                remainder = (count % 10) 
+
+                if remainder >= 5:
+                    total += (remainder // 5) * 45 
+                    remainder = remainder % 5
+
+                total += remainder * items[item]
 
             else:
                 total += count * items[item]
 
         return total
+
 
