6836a9718a2653b7d9d14085c687f50f28737bbe
sbx_user1051 <sbx_user1051@169.254.41.1>
2025-04-06 17:06:32 +0000

    Sun Apr 06 17:06:32 UTC 2025

--- test/solution_tests/CHK/test_chk1.py
+++ test/solution_tests/CHK/test_chk1.py
@@ -8,6 +8,11 @@ class TestCheckout():
     def test_checkout2_correct(self):
         assert CheckoutSolution().checkout("EEEBBE")==160
 
+    def test_checkout2_correct(self):
+        assert CheckoutSolution().checkout("ABCDECBAABCABBAAAEEAA")==665
+        
+
     def test_checkout1_incorrect(self):
         assert CheckoutSolution().checkout("a")==-1
+
 
